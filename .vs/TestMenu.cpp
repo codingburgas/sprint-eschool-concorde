4a3041953d482c51543dd43991c7d62749e04227
Bojidar Georgiev <BMGeorgiev23@codingburgas.bg>
2025-03-07 22:57:33 +0200

    Add raylib to Start Menu/Test Selection

--- .vs/TestMenu.cpp
+++ .vs/TestMenu.cpp
@@ -1,52 +1,36 @@
-#include <iostream>
-#include <string>
+#include "raylib.h"
 #include "testmenu.h"
 #include "symbolistTest.h"
 #include "SpleenTest.h"
 #include "ConsoleApplication1/ConsoleApplication1/AutumnSongTest.h"
 
-using namespace std;
-
 void testmenu() {
-    system("CLS");
-    cout << "-/-----------------------------------------------------------------------------------------------------------------/-" << endl;
-    cout << "    _________ _______  _______ _________   _______  _______  _______ __________________ _______  _       " << endl;
-    cout << "    \\__   __/(  ____ \\(  ____ \\\\__   __/  (  ____ \\(  ____ \\(  ____ \\\\__   __/\\__   __/(  ___  )( (    /|" << endl;
-    cout << "       ) (   | (    \\/| (    \\/   ) (     | (    \\/| (    \\/| (    \\/   ) (      ) (   | (   ) ||  \\  ( |" << endl;
-    cout << "       | |   | (__    | (_____    | |     | (_____ | (__    | |         | |      | |   | |   | ||   \\ | |" << endl;
-    cout << "       | |   |  __)   (_____  )   | |     (_____  )|  __)   | |         | |      | |   | |   | || (\\ \\) |" << endl;
-    cout << "       | |   | (            ) |   | |           ) || (      | |         | |      | |   | |   | || | \\   |" << endl;
-    cout << "       | |   | (____/\\/\\____) |   | |     /\\____) || (____/\\| (____/\\   | |   ___) (___| (___) || )  \\  |" << endl;
-    cout << "       )_(   (_______/\\_______)   )_(     \\_______)(_______/(_______/   )_(   \\_______/(_______)|/    )_)" << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "                                      CHOOSE ONE OF THE FOLLOWING TESTS:  " << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "                                          1. Symbolist Movement" << endl;
-    cout << "                                       2. Spleen - Charles Baudelaire" << endl;
-    cout << "                                       3. Autumn Song - Paul Verlaine" << endl;
-    cout << "                                           4. Return to Main Menu" << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "                                                                                                              " << endl;
-    cout << "-/-----------------------------------------------------------------------------------------------------------------/-" << endl;
-    int optionTestMenu;
-    cin >> optionTestMenu;
+    int selectedOption = 0;
+    bool exitMenu = false;
 
-    if (optionTestMenu == 1) {
-        SymbolistTest();
-    }
-    else if (optionTestMenu == 2) {
-        SpleenTest();
-    }
-    else if (optionTestMenu == 3) {
-        AutumnSongTest();
-    }
-    else if (optionTestMenu == 4) {
-        return;
-    }
-    else {
-        system("CLS");
-        cout << "Invalid input, try again!" << endl;
+    while (!exitMenu) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("CHOOSE ONE OF THE FOLLOWING TESTS:", 150, 50, 20, DARKBLUE);
+        DrawText("1. Symbolist Movement", 200, 150, 20, BLACK);
+        DrawText("2. Spleen - Charles Baudelaire", 200, 200, 20, BLACK);
+        DrawText("3. Autumn Song - Paul Verlaine", 200, 250, 20, BLACK);
+        DrawText("4. Return to Main Menu", 200, 300, 20, BLACK);
+
+        if (IsKeyPressed(KEY_ONE)) {
+            SymbolistTest();
+        }
+        else if (IsKeyPressed(KEY_TWO)) {
+            SpleenTest();
+        }
+        else if (IsKeyPressed(KEY_THREE)) {
+            AutumnSongTest();
+        }
+        else if (IsKeyPressed(KEY_FOUR)) {
+            exitMenu = true;
+        }
+
+        EndDrawing();
     }
 }
